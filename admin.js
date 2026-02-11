380e54fa078571fedec924b2b60cd165fb48c721
Jackson tech <jacksonlumumba275@gmail.com>
2026-02-11 15:45:21 +0300

    Create admin.js

---
+++ admin.js
@@ -0,0 +1,8 @@
+let requests = JSON.parse(localStorage.getItem("requests")) || [];
+let container = document.getElementById("requestList");
+requests.forEach(r => {
+  let card = document.createElement("div");
+  card.className = "domain-card";
+  card.innerHTML = `<h3>${r.domain}</h3><p>${r.business}</p><p>${r.email}</p><p>${r.details}</p>`;
+  container.appendChild(card);
+});
