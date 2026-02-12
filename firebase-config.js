213736fe778ddd73f1f88479752f78d8655bc923
Jackson tech <jacksonlumumba275@gmail.com>
2026-02-12 10:55:48 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,12 @@
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "PrimeDomains.firebaseapp.com",
-  projectId: "primedomains-63842",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
-};
+    apiKey: "AIzaSyD6L_W_FaJ4TI_VnToB26y3xdzTM9y_0LQ",
+    authDomain: "primedomains-63842.firebaseapp.com",
+    projectId: "primedomains-63842",
+    storageBucket: "primedomains-63842.firebasestorage.app",
+    messagingSenderId: "790086407326",
+    appId: "1:790086407326:web:8016a6ce0a8c9aa6bfd78f",
+    measurementId: "G-55N0G7ZK5T
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
